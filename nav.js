2c59f66c19d2f2429ca240a48dc97926228586c3
Mathieu <larrouy.m@me.com>
2021-01-22 16:12:53 +0100

    trigger script

--- nav.js
+++ nav.js
@@ -1,17 +1,31 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
 
-// // Get the navbar
-// var navigation = document.getElementById("navigation");
+// (function() {
+// 	scrollTo();
+// })();
 
-// // Get the offset position of the navbar
-// var sticky = navigation.offsetTop;
+// function scrollTo() {
+// 	const links = document.querySelectorAll('.ul_second li a');
+// 	links.forEach(each => (each.onclick = scrollAnchors));
+// }
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageXOffset >= sticky) {
-//     navigation.classList.add("sticky")
-//   } else {
-//     navigation.classList.remove("sticky");
-//   }
+// function scrollAnchors(e, respond = null) {
+// 	const distanceToLeft = el => Math.floor(el.getBoundingClientRect().left);
+// 	e.preventDefault();
+// 	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+//     const targetAnchor = document.querySelector(targetID);
+//     console.log(targetID);
+// 	if (!targetAnchor) return;
+// 	const originalLeft = distanceToLeft(targetAnchor);
+// 	window.scrollBy({ left: originalLeft, top: 0, behavior: 'smooth' });
+// 	const checkIfDone = setInterval(function() {
+// 		const atRight = window.innerWidth + window.pageXOffset >= document.body.offsetWidth - 2;
+// 		if (distanceToLeft(targetAnchor) === 0 || atRight) {
+// 			targetAnchor.tabIndex = '-1';
+// 			targetAnchor.focus();
+// 			window.history.pushState('', '', targetID);
+// 			clearInterval(checkIfDone);
+// 		}
+// 	}, 100);
 // }
---
+++ trigger2.js
@@ -0,0 +1,18 @@
+const trigger2 = document.querySelector('.trigger_2');
+const img2 = document.querySelectorAll('.img_2');
+
+const T2L = gsap.timeline({paused: true});
+
+T2L 
+.to(img2, {scale: 1, duration: 0.4, stagger: 0.1, ease: "back.out(1.7)"});
+
+trigger2.addEventListener('mouseenter', () => {
+
+    T2L.play();
+
+})
+trigger2.addEventListener('click', () => {
+
+    T2L.reverse();
+
+})
