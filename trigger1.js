7844ac797c77415b159620f18f1d7583c840146d
Mathieu <larrouy.m@me.com>
2021-01-24 16:08:27 +0100

    logo fe

--- trigger1.js
+++ trigger1.js
@@ -1,25 +1,15 @@
-const trigger1 = document.querySelector('.trigger_1');
-const img1 = document.querySelectorAll('.img_1');
+const trigger1 = document.querySelector(".trigger_1");
+const img1 = document.querySelectorAll(".img_1");
+const TL = gsap.timeline({ paused: true });
 
-const TL = gsap.timeline({paused: true});
+TL.to(img1, { scale: 1, duration: 0.4, stagger: 0.1, ease: "back.out(1.7)" });
 
-TL 
-.to(img1, {scale: 1, duration: 0.4, stagger: 0.1, ease: "back.out(1.7)"});
-
-trigger1.addEventListener('mouseenter', () => {
-
-    TL.play();
-
-})
-trigger1.addEventListener('touchstart', () => {
-
-    TL.play();
-
-})
-trigger1.addEventListener('click', () => {
-
-    TL.reverse();
-
-})
-
-
+trigger1.addEventListener("mouseenter", () => {
+  TL.play();
+});
+trigger1.addEventListener("touchstart", () => {
+  TL.play();
+});
+trigger1.addEventListener("click", () => {
+  TL.reverse();
+});
--- trigger2.js
+++ trigger2.js
@@ -1,18 +1,15 @@
-const trigger2 = document.querySelector('.trigger_2');
-const img2 = document.querySelectorAll('.img_2');
+const trigger2 = document.querySelector(".trigger_2");
+const img2 = document.querySelectorAll(".img_2");
+const T2L = gsap.timeline({ paused: true });
 
-const T2L = gsap.timeline({paused: true});
+T2L.to(img2, { scale: 1, duration: 0.4, stagger: 0.1, ease: "back.out(1.7)" });
 
-T2L 
-.to(img2, {scale: 1, duration: 0.4, stagger: 0.1, ease: "back.out(1.7)"});
-
-trigger2.addEventListener('mouseenter', () => {
-
-    T2L.play();
-
-})
-trigger2.addEventListener('click', () => {
-
-    T2L.reverse();
-
-})
+trigger2.addEventListener("mouseenter", () => {
+  T2L.play();
+});
+trigger2.addEventListener("touchstart", () => {
+  T2L.play();
+});
+trigger2.addEventListener("click", () => {
+  T2L.reverse();
+});
