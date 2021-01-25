b680ce1a074abf6bdb486b2631421809a93ec9ba
Mathieu <larrouy.m@me.com>
2021-01-25 16:33:27 +0100

    mobile

--- burger.js
+++ burger.js
@@ -1,44 +1,36 @@
 function navSlide() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".ul_second");
-    const navLinks = document.querySelectorAll(".ul_second li a");
-    
-    burger.addEventListener("click", () => {
-        //Toggle Nav
-        nav.classList.toggle("nav-active");
-        
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ""
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        // Burger Animation
-        burger.classList.toggle("toggle");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".ul_second");
+  const navLinks = document.querySelectorAll(".ul_second li a");
 
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
-   
-    
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
 }
 
 navSlide();
-// const navLinks = document.querySelectorAll(".ul_second li a");
-// navLinks.on("click", function(){
-//     $('burger').click();
-//   });
 
-// function navHide() {
-//     const toggles = document.querySelector(".toggle");
-//     const navLinks = document.querySelectorAll(".ul_second li a");
-    
-//     navLinks.addEventListener("click", () => {
-//         //Toggle Nav
-//         toggles.classList.toggle("burger");
+function navHide() {
+  const toggles = document.querySelector(".toggle");
+  const navLink = document.querySelectorAll(".ul_second li a");
 
-//     });
-   
-    
-// }
-// navHide();
+  navLink.addEventListener("click", () => {
+    //Toggle Nav
+    toggles.classList.toggle("burger");
+  });
+}
+navHide();
--- mousseover.js
+++ mousseover.js
@@ -1,15 +1,15 @@
-let balls = document.getElementsByClassName("zero");
+// let balls = document.getElementsByClassName("zero");
 
-document.onmousemove = function () {
-  let x = (window.event.clientX * 20) / window.innerWidth + "%";
-  let y = (window.event.clientY * 20) / window.innerHeight + "%";
+// document.onmousemove = function () {
+//   let x = (window.event.clientY * 10) / window.innerWidth + "%";
+//   let y = (window.event.clientX * 5) / window.innerHeight + "%";
 
-  for (let i = 0; i < 2; i++) {
-    balls[i].style.left = x;
-    balls[i].style.top = y;
-    balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
-  }
+//   for (let i = 0; i < 2; i++) {
+//     balls[i].style.left = x;
+//     balls[i].style.top = y;
+//     balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
+//   }
 
-  console.log("x =" + x);
-  console.log("y =" + y);
-};
+//   console.log("x =" + x);
+//   console.log("y =" + y);
+// };
