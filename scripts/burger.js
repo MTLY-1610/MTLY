85d728e2c63a32da94186984af8ba665fb305f5d
Mathieu <larrouy.m@me.com>
2021-01-26 16:49:28 +0100

    burger fixed

--- scripts/burger.js
+++ scripts/burger.js
@@ -2,12 +2,9 @@ function navSlide() {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".ul_second");
   const navLinks = document.querySelectorAll(".ul_second li a");
-
-  burger.addEventListener("click", () => {
-    //Toggle Nav
+  nav.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-
-    // Animate Links
+    burger.classList.toggle("toggle");
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
@@ -17,9 +14,22 @@ function navSlide() {
         }s`;
       }
     });
+  });
+  burger.addEventListener("click", () => {
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+    // Animate Links
     // Burger Animation
     burger.classList.toggle("toggle");
   });
 }
-
 navSlide();
